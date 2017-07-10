5f747a415caa0cb5ae01a1f91f7147eac8447f5c
Aify <hlo1453@gmail.com>
2017-07-10 11:29:05 -0700

    init 3

---
+++ src/core/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package core;
+
+/**
+ * @author Aify
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
